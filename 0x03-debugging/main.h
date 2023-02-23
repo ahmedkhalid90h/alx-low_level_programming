e4b9e684e8d3d508e824e9ec0bd1d4416e790b89
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-02-23 20:30:07 +0200

    task-0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,19 +1,11 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
 #include <stdio.h>
 
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 
-#endif
+#endif /* HOLBERTON_H */
