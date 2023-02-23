91f3af186c4fd174a97447b49b1d36305358e02e
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-02-23 20:31:16 +0200

    task-0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,11 +1,8 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-void print_remaining_days(int month, int day, int year);
-int convert_day(int month, int day);
 
-#endif /* HOLBERTON_H */
+#endif /* MAIN_H */
