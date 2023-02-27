6ffdf2ffe0f7a2fcbdd00b806a15b5f80708c633
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-02-27 23:44:39 +0200

    t

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -22,16 +22,17 @@ return (len);
 **/
 void puts_half(char *str)
 {
+int i ;
 if (_strlen(str) % 2 == 0)
 {
-for (int i = _strlen(str) / 2; str[i] != '\0'; i++)
+for ( i = _strlen(str) / 2; str[i] != '\0'; i++)
 {
 _putchar(str[i]);
 }
 }
 else
 {
-for (int i = ((_strlen(str) / 2) + 1);str[i] != '\0'; i++)
+for ( i = ((_strlen(str) / 2) + 1); str[i] != '\0'; i++)
 {
 _putchar(str[i]);
 }
