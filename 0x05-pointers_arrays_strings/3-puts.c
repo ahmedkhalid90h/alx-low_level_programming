230ba9505ebfa60dfac7bc7e20f6618bc68c9aa1
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-02-27 20:55:08 +0200

    task

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *  _puts - counts the number of characters
+ *  @str:string character
+ * Return: Always 0.
+ */
+
+void _puts(char *str)
+{
+printf("%s\n",str);
+}
