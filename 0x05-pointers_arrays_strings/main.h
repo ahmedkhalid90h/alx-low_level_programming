963dca0a10b5a3dc3709ff9cbe88ff16de41b12b
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-02-28 00:13:59 +0200

    ss

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,5 +8,7 @@ int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 
 #endif /* HOLBERTON_H */
