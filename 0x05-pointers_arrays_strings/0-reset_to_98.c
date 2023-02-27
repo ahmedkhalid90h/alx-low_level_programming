2ce82872589713656ebe8091383d5b9d452bb4d8
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-02-27 19:45:24 +0200

    task

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -6,16 +6,18 @@
  *
  * Return: Always 0.
  */
-void reset_to_98(int *n){
-   *n = 98;
+
+void reset_to_98(int *n) {
+*n = 98;
 }
+
 int main(void)
 {
-    int n;
+int n;
 
-    n = 402;
-    printf("n=%d\n", n);
-    reset_to_98(&n);
-    printf("n=%d\n", n);
-    return (0);
+n = 402;
+printf("n=%d\n", n);
+reset_to_98(&n);
+printf("n=%d\n", n);
+return (0);
 }
