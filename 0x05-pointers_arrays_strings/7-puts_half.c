32ab7d335071385a755fc2e1f888dfa9b00be4e1
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-02-27 23:32:04 +0200

    tas

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+* _strlen - function that returns the length of a string.
+* @s : s is a character
+* Return: value is i
+**/
+
+int _strlen(char *s)
+{
+int len = 0;
+while (*s++)
+len++;
+return (len);
+}
+
+/**
+ * puts_half - unction that prints half of a string, followed by a new line.
+ * character, followed by a new line
+ * @str: pointer to a string
+ * Return: void
+**/
+void puts_half(char *str)
+{
+if (_strlen(str) % 2 == 0)
+{
+for (int i = _strlen(str) / 2; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+}
+else
+{
+for (int i = _strlen(str) / 2 + 1;str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+}
+_putchar('\n');
+}
