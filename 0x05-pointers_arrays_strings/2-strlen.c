a73e1b3358a098fd361ffba8ec56f27a73007308
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-02-28 00:16:49 +0200

    s

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,15 @@
 #include "main.h"
 
 /**
- *  _strlen - counts the number of characters
- *  @s:string character
- * Return: Always 0.
- */
+ * _strlen - returns the length of a string
+ * @s: pointer to a string
+ * Return: length of a string
+**/
 int _strlen(char *s)
 {
-	int len = 0;
-	while (*s != '\0')
-	{
-		len++;
-		s++;
-	}
-	return (len);
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
 }
