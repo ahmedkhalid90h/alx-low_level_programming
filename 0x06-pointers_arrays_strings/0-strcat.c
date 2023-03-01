2a0e7cfb5f0121dd0c105bd58232fb963b01ae36
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-03-01 17:51:13 +0200

    fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,14 +9,14 @@
 char *_strcat(char *dest, char *src)
 {
 int i =0, j;
-char *p = dest;
-while (*p != '\0')
-p++;
+char *ptr = dest;
+while (*ptr != '\0')
+ptr++;
 while (*src!='\0'){
-*p= *src;
-p++;
+*ptr= *src;
+ptr++;
 src++;
 }
-*p='\0';
+*ptr='\0';
 return(dest);
 }
