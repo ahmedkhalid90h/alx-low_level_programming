384cca6bb48dc0e6aef981457847599d03fa8048
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-03-01 18:00:33 +0200

    ss

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,15 +1,6 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-char *_strcat(char *dest, char *src);
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-void print_rev(char *s);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
+char _strcat(char *dest, char *src);
 
 #endif /* HOLBERTON_H */
