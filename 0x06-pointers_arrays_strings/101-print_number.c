17e1d55888070a784ae6c064ffc46c4fb9be2056
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-03-02 22:44:51 +0200

    fixx

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,18 +1,24 @@
-#include "main.h"
 
 /**
  * print_number - prints an integer
  * @n: the integer to print
+ *
+ * Return: void
  */
 void print_number(int n)
 {
+int divisor = 1;
+
 if (n < 0) {
 _putchar('-');
 n = -n;
 }
 
-if (n / 10 != 0)
-print_number(n / 10);
+while (n / divisor > 9)
+divisor *= 10;
 
-_putchar(n % 10 + '0');
+while (divisor != 0) {
+_putchar((n / divisor) % 10 + '0');
+divisor /= 10;
+}
 }
