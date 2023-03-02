eee6059648a928965d16db4051bf825cb616e154
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-03-02 08:03:53 +0200

    commit

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_strcat - a function that concatenates two strings
+ * @dest: string
+ * @src: string
+ * @n:integer
+ * Return: string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+    char *ptr = dest;
+    int i;
+    while (*ptr != '\0')
+        *ptr++;
+    while (*src != '\0' && n-- >= 0)
+    {
+        *ptr++ = *src++;
+    }
+    *ptr = '\0';
+    return (dest);
+}
