6ad5b2aa0d3960d194e2adbfc370f6f35bf33995
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-03-02 08:05:48 +0200

    commit-m

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,14 +9,14 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-    char *ptr = dest;
-    int i;
-    while (*ptr != '\0')
-        *ptr++;
-    while (*src != '\0' && n-- >= 0)
-    {
-        *ptr++ = *src++;
-    }
-    *ptr = '\0';
-    return (dest);
+char *ptr = dest;
+int i;
+while (*ptr != '\0')
+*ptr++;
+while (*src != '\0' && n-- >= 0)
+{
+*ptr++ = *src++;
+}
+*ptr = '\0';
+return (dest);
 }
