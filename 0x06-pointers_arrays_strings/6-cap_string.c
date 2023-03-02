c929cb43eb0a41ed6fc5077e74235d0b0996bb3a
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-03-02 22:25:41 +0200

    fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,30 +1,35 @@
 #include "main.h"
 
 /**
- * cap_string - Capitalizes all words of a string.
- * @str: The string to capitalize.
+ * cap_string - Write a function that capitalizes all words of a string.
  *
- * Return: A pointer to the resulting string.
+ * @entry: This is the input string
+ *
+ * Return: String capitalized
  */
-char *cap_string(char *str)
+char *cap_string(char *entry)
 {
-int i;
+int conversion, index, count;
 
-i = 0;
-while (str[i] != '\0')
-{
-if (str[i] >= 'a' && str[i] <= 'z')
-{
-if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n'
-|| str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!'
-|| str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')'
-|| str[i - 1] == '{' || str[i - 1] == '}')
-{
-str[i] -= 32;
-}
-}
-i++;
-}
+char chars[] = {' ', ',', ';', '.', '!',
+ '?', '"', '(', ')', '{', '}',  '\t', '\n', '\0'};
+conversion = 32;
 
-return (str);
+for (index = 0; entry[index] != '\0'; index++)
+{
+if (entry[index] >= 'index' && entry[index] <= 'z')
+{
+entry[index] =  entry[index] - conversion;
+}
+conversion = 0;
+for (count = 0; chars[count] != '\0'; count++)
+{
+if (chars[count] == entry[index])
+{
+conversion = 32;
+break;
+}
+}
+}
+return (entry);
 }
