8438488b4b1473cc1283b6fb60562a327e172dc2
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-03-02 22:48:51 +0200

    fix

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,7 +9,8 @@ void print_number(int n)
 {
 int divisor = 1;
 
-if (n < 0) {
+if (n < 0)
+{
 _putchar('-');
 n = -n;
 }
@@ -17,7 +18,8 @@ n = -n;
 while (n / divisor > 9)
 divisor *= 10;
 
-while (divisor != 0) {
+while (divisor != 0)
+{
 _putchar((n / divisor) % 10 + '0');
 divisor /= 10;
 }
