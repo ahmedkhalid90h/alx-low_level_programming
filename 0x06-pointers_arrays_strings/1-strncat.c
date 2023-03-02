978aa294f79530d5675902e939fe796d09e00302
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-03-02 20:51:46 +0200

    fixxxxxxx

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,7 +7,6 @@
  * @dest: This is the output  dest
  * @src: This is the input src
  * @n: This is the number bytes fro src
- *
  * Return: This is my return
  */
 char *_strncat(char *dest, char *src, int n)
