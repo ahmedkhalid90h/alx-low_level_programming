5022081209839235d5ba4efd1bc642e96358551d
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-03-02 08:08:04 +0200

    fix

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* *_strcat - a function that concatenates two strings
+* _strcat - a function that concatenates two strings
  * @dest: string
  * @src: string
  * @n:integer
