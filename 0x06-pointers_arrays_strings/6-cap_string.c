b9347c452b616c12eca1545c92e487fc67cbd32b
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-03-02 22:27:45 +0200

    fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,23 +8,23 @@
  */
 char *cap_string(char *str)
 {
-    int i;
+int i;
 
-    i = 0;
-    while (str[i] != '\0')
-    {
-        if (str[i] >= 'a' && str[i] <= 'z')
-        {
-            if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n'
-                    || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!'
-                    || str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')'
-                    || str[i - 1] == '{' || str[i - 1] == '}')
-            {
-                str[i] -= 32;
-            }
-        }
-        i++;
-    }
+i = 0;
+while (str[i] != '\0')
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n'
+|| str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!'
+|| str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')'
+|| str[i - 1] == '{' || str[i - 1] == '}')
+{
+str[i] -= 32;
+}
+}
+i++;
+}
 
-    return (str);
+return (str);
 }
