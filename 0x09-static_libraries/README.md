85d2265a842fd57e45cbcee33a65fcc1cb3f10db
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-03-10 20:58:15 +0200

    f

--- 0x09-static_libraries/README.md
+++ 0x09-static_libraries/README.md
@@ -1,1 +1,8 @@
-README.md exists and is not empty
+# libmy.a
+the static library libmy.a containing all the functions listed in [main.h]
+
+# main.h 
+header file 
+
+# create_static_lib.sh
+that script creates a static library called liball.a from all the .c files that are in the current directory
---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - entry point
+* @argc: size of argv
+* @argv: array
+* Return: 0
+**/
+
+int main(int argc, char **argv)
+{
+	int cents, coins;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	coins = 0;
+	coins += cents / 25;
+	cents %= 25;
+	coins += cents / 10;
+	cents %= 10;
+	coins += cents / 5;
+	cents %= 5;
+	coins += cents / 2;
+	cents %= 2;
+	coins += cents;
+
+	printf("%d\n", coins);
+
+	return (0);
+}
