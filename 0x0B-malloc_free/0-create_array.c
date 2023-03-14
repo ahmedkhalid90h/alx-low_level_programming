d34a6856eb170d2bf738b833b790ab096c34c7cf
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-03-14 16:16:11 +0200

    fox

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,14 +13,14 @@ char *array;
 unsigned int i;
 if (size == 0)
 {
-return NULL;
+return (NULL);
 }
 array = malloc(size * sizeof(char));
 if (array == NULL)
 {
-return NULL;
+return (NULL);
 }
-for ( i = 0; i < size; i++)
+for (i = 0; i < size; i++)
 array[i] = c;
 
 return (array);
