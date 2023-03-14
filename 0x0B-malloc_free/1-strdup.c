1724a3311ada214f195d6db24ece6ef739744ecf
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-03-14 17:00:06 +0200

    f

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -6,6 +6,7 @@
  * which contains a copy of the string given as a parameter.
  * @str:pointer string
  * Return: a pointerto to a newly allocated space in memory.
+ * It returns NULL if insufficient memory was available
 */
 
 char *_strdup(char *str)
@@ -16,7 +17,7 @@ if (str == NULL)
 return (NULL);
 while (str[len] != '\0')
 len++;
-dup_str = (char *)malloc((sizeof(char) * len) + 1);
+dup_str = (char *)malloc((sizeof(char) * len) +1);
 if (dup_str == NULL)
 return (NULL);
 for (i = 0; i < len; i++)
