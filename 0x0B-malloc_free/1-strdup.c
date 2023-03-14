ff597e048d0dab5bdaa5a7e3644ff77fca2e7b01
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-03-14 16:59:17 +0200

    push

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,12 +11,12 @@
 char *_strdup(char *str)
 {
 char *dup_str;
-int i,len = 0;
+int i, len = 0;
 if (str == NULL)
 return (NULL);
 while (str[len] != '\0')
 len++;
-dup_str = (char *)malloc((sizeof(char) * len)+ 1);
+dup_str = (char *)malloc((sizeof(char) * len) + 1);
 if (dup_str == NULL)
 return (NULL);
 for (i = 0; i < len; i++)
