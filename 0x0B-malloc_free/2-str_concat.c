fe3607f07710427cbeb99f9c4ea75813e1787e86
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-03-14 18:39:57 +0200

    fixxxx

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,14 +15,30 @@ char *str_concat(char *s1, char *s2)
 {
 char *concat;
 int i,j,lenTotal,len1,len2;
+if (s1 != NULL)
+{
 while(s1[len1] !='\0')
+i = 0;
 len1++;
+}
+if (s2 != NULL)
+{
 while(s2[len2] !='\0')
+i = 0;
 len2++;
+}
 lenTotal = len1 + len2;
 concat =(char *)malloc(sizeof(char) * (lenTotal + 1));
 if (concat == NULL)
 return (NULL);
-
+for (i; i < len1; i++)
+{
+concat[i] = s1[i];
+}
+for (j; j < len2; j++, i++)
+{
+concat[i] = s2[j];
+}
+concat = '\0';
 return (concat);
 }
