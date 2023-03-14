94792e6e8a3e5e2126d68348a46cba00b544a265
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-03-14 18:43:22 +0200

    tasks

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,28 +14,26 @@
 char *str_concat(char *s1, char *s2)
 {
 char *concat;
-int i,j,lenTotal,len1,len2;
+int i=0, j=0, lenTotal, len1 = 0, len2 = 0;
 if (s1 != NULL)
 {
-while(s1[len1] !='\0')
-i = 0;
+while (s1[len1] != '\0')
 len1++;
 }
 if (s2 != NULL)
 {
-while(s2[len2] !='\0')
-i = 0;
+while (s2[len2] != '\0')
 len2++;
 }
 lenTotal = len1 + len2;
-concat =(char *)malloc(sizeof(char) * (lenTotal + 1));
+concat = (char *)malloc(sizeof(char) * (lenTotal + 1));
 if (concat == NULL)
 return (NULL);
-for (i; i < len1; i++)
+for (; i < len1; i++)
 {
 concat[i] = s1[i];
 }
-for (j; j < len2; j++, i++)
+for (; j < len2; j++, i++)
 {
 concat[i] = s2[j];
 }
