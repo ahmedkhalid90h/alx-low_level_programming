99e3fbbb8dc73297613d6a4ba65893b953ee9b17
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-03-14 23:47:54 +0200

    fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,56 +2,39 @@
 #include <stdlib.h>
 
 /**
- * str_concat - concatenates two strings.
- * @s1: the first string
- * @s2: the second string
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers.
+ * @width: width of array
+ * @height: height of array
  *
- * Return: a pointer to a newly allocated space in memory which
- * contains the contents of s1, followed by the contents of s2,
- * and null terminated. NULL on failure
+ * Return: a pointer to a 2 dimensional array of integers.
  */
-
 int **alloc_grid(int width, int height)
 {
+	int **array, i, j;
+	int len = width * height;
 
-}
-void print_grid(int **grid, int width, int height)
-{
-    int w;
-    int h;
+	if (len <= 0)
+		return (NULL);
 
-    h = 0;
-    while (h < height)
-    {
-        w = 0;
-        while (w < width)
-        {
-            printf("%d ", grid[h][w]);
-            w++;
-        }
-        printf("\n");
-        h++;
-    }   
-}
+	array = (int **)malloc(sizeof(int *) * height);
+	if (array == NULL)
+		return (NULL);
 
-/**
- * main - check the code for ALX School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int **grid;
+	for (i = 0; i < height; i++)
+	{
+		array[i] = (int *)malloc(sizeof(int) * width);
+		if (array[i] == NULL)
+		{
+			for (i--; i >= 0; i--)
+				free(array[i]);
+			free(array);
+			return (NULL);
+		}
+	}
 
-    grid = alloc_grid(6, 4);
-    if (grid == NULL)
-    {
-        return (1);
-    }
-    print_grid(grid, 6, 4);
-    printf("\n");
-    grid[0][3] = 98;
-    grid[3][4] = 402;
-    print_grid(grid, 6, 4);
-    return (0);
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			array[i][j] = 0;
+
+	return (array);
 }
