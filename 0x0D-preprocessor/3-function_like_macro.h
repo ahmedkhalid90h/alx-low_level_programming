43b8ded0a7e5099399f28656648579286ea35179
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-03-20 16:51:33 +0200

    fix

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
 #ifndef _function_like_macro_h_
 #define _function_like_macro_h_
 
-#define ABS(x) (((x)<0)? -(x) : (x))
+#define ABS(x) (((x) < 0) ? -(x) : (x))
 
 #endif /* _function_like_macro_h_ */
