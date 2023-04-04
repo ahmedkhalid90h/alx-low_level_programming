85f7343817bdb4af845233731be70bb52e074766
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-04-04 10:19:05 +0200

    s

--- 0x13-more_singly_linked_lists.git/0-print_listint.c
+++ 0x13-more_singly_linked_lists.git/0-print_listint.c
@@ -3,9 +3,11 @@
 #include <stdio.h>
 
 /**
- * @print_listint:fun return number of node
- * @count:count number of node
-*/
+ * print_listint - prints all the elements of a linked list
+ * @h: linked list of type listint_t to print
+ *
+ * Return: number of nodes
+ */
 
 size_t print_listint(const listint_t *h)
 {
