8383219b63c52ebc25913cfb69ba88406236bdda
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-04-04 10:35:27 +0200

    pull

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_listint - prints all the elements of a linked list
+ * @h: linked list of type listint_t to print
+ *
+ * Return: number of nodes
+ */
+
+size_t listint_len(const listint_t *h)
+{
+size_t count;
+
+count = 0;
+
+while (h)
+{
+count += 1;
+h = h->next;
+}
+return (count);
+}
