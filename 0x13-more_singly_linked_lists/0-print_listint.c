4b1bc8f1f275989cf959c9fb38e0d68164321e30
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-04-05 00:58:04 +0200

    f

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,7 +5,6 @@
 /**
  * print_listint - prints all the elements of a linked list
  * @h: linked list of type listint_t to print
- *
  * Return: number of nodes
  */
 
@@ -21,6 +20,5 @@ printf("%i\n", h->n);
 h = h->next;
 count += 1;
 }
-
 return (count);
 }
