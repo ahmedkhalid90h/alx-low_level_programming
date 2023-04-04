5f6bb7ffc92fae22cf01ee6dc63bcb74deee3cb4
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-04-04 21:20:02 +0200

    push

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * pop_listint - Realease the memory allocated for a list
+ *
+ * @head: A pointer to the first node of the list to free
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+unsigned int count;
+
+count = 0;
+
+while (count < index)
+{
+if (head == NULL)
+return (NULL);
+
+head = head->next;
+count++;
+}
+return (head);
+}
---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * sum_listint - Realease the memory allocated for a list
+ *
+ * @head: A pointer to the first node of the list to free
+ */
+
+int sum_listint(listint_t *head)
+{
+unsigned int i;
+i = 0;
+if (head == NULL)
+return (0);
+while (head)
+{
+i += head->n;
+head = head->next;
+}
+
+return (i);
+}
