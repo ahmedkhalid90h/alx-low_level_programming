731a8ddbf432ba94e315163f20da3241cbeea137
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-04-11 00:45:29 +0200

    gp

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,15 +1,27 @@
 #include "main.h"
 
 /**
- * get_bit - prints all the elements of a linked list
- * @n: linked list of type listint_t to print
- * @index: linked list of type listint_t to print
+ * append_text_to_file - prints all the elements of a linked list
+ * @filename: linked list of type listint_t to print
+ * @text_content: linked list of type listint_t to print
  * Return: number
  */
 
-int get_bit(unsigned long int n, unsigned int index)
+int append_text_to_file(const char *filename, char *text_content)
 {
-if (index > sizeof(n) * 8)
+int fd, len = 0;
+
+if (filename == NULL)
 return (-1);
-return ((n >> index) & 1);
+fd = open(filename, O_WRONLY | O_APPEND);
+if (fd == -1)
+return (-1);
+if (text_content != NULL)
+{
+while (text_content[len])
+len++;
+write(fd, text_content, len);
+}
+close(fd);
+return (1);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,9 +5,9 @@
 #include <fcntl.h>
 #include <stdlib.h>
 
+ssize_t read_textfile(const char *filename, size_t letters);
 int append_text_to_file(const char *filename, char *text_content);
 int create_file(const char *filename, char *text_content);
-ssize_t read_textfile(const char *filename, size_t letters);
 
 
 #endif
