ba6e1d07b2bdc90d575baf23f5ae22d6ac4e92b7
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-04-10 02:15:10 +0200

    go to hele

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,18 +3,18 @@
 /**
  * binary_to_uint - prints all the elements of a linked list
  * @b: linked list of type listint_t to print
- * Return: number 
+ * Return: number
  */
 
 unsigned int binary_to_uint(const char *b)
 {
 unsigned int nu = 0;
 int i = 0;
-if(!b) 
+if (!b)
 return (0);
 while (b[i] != '\0')
 {
-if (b[i] != '0'&& b[i] != '1')
+if (b[i] != '0' && b[i] != '1')
 return (0);
 nu <<= 1;
 if (b[i] == '1')
