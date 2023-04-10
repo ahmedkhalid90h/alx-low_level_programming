50e6369a41f269f1a075b506cb6e90396a69d065
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-04-11 00:32:10 +0200

    gg

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * get_endianness - prints all the elements of a linked list
- * Return: number
+ * get_endianness - checks the endianness
+ * Return: 0 if big endian, 1 if little endian
  */
-
 int get_endianness(void)
 {
 unsigned int i = 1;
