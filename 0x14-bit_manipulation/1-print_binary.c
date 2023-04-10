606f583f7cd428fd1264984a4a32364b850e6c3e
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-04-11 00:22:22 +0200

    go

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,14 +1,27 @@
 #include "main.h"
 
 /**
- * print_binary - prints all the elements of a linked list
- * @n: linked list of type listint_t to print
- * Return: number
+ * create_file - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file to read
+ * @text_content: number of letters it should read and print
+ *
+ * Return: actual number of letters it could read and print
  */
 
-void print_binary(unsigned long int n)
+int create_file(const char *filename, char *text_content)
 {
-if (n > 1)
-print_binary(n >> 1);
-putchar((n & 1) + '0');
+int fCheck, len = 0;
+if (filename == NULL)
+return (0);
+fCheck = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+if (fCheck == -1)
+return (-1);
+if (text_content != NULL)
+{
+while (text_content[len])
+len++;
+write(fCheck, text_content, len);
+}
+close(fCheck);
+return (-1);
 }
